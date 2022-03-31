0604cdde84523640930a18f9e6fbd46047567c85
anushkrishnav <anush.venkatakrishna@gmail.com>
2022-03-31 18:46:18 +0530

    added test

--- Main/Routes/routes.py
+++ Main/Routes/routes.py
@@ -2,8 +2,13 @@ from Main.api.userapi import UserApi, UsersApi, UserImageApi, Checkemail
 
 from Main.api.authapi import SignupApi, LoginApi
 
+
+class testApi(Resource):
+    def get(self):
+        return "test"
 def initialize_routes(api):
     # List users
+    api.add_resource(testApi, '/')
     api.add_resource(UsersApi, '/api/check/<username>') 
     api.add_resource(Checkemail, '/api/check/mail/<email>') 
     api.add_resource(UserApi, '/api/user')
