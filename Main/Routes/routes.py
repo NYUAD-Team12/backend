9e8726da1b9964f7462280ec90c53b5a41a25097
anushkrishnav <anush.venkatakrishna@gmail.com>
2022-03-31 18:57:36 +0530

    fix idk maybe

--- Main/Routes/routes.py
+++ Main/Routes/routes.py
@@ -1,14 +1,9 @@
 from Main.api.userapi import UserApi, UsersApi, UserImageApi, Checkemail
-
 from Main.api.authapi import SignupApi, LoginApi
 
 
-class testApi(Resource):
-    def get(self):
-        return "test"
 def initialize_routes(api):
     # List users
-    api.add_resource(testApi, '/')
     api.add_resource(UsersApi, '/api/check/<username>') 
     api.add_resource(Checkemail, '/api/check/mail/<email>') 
     api.add_resource(UserApi, '/api/user')
