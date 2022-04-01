cb8f33fe58b2f6f999ada9da063179d1da8302ef
anushkrishnav <anush.venkatakrishna@gmail.com>
2022-04-01 15:57:04 +0530

    reverse

--- test.py
+++ test.py
@@ -12,12 +12,12 @@ base = "http://127.0.0.1:5000/api/"
 base2 = "https://resq-api.azurewebsites.net/api/"
 # res = requests.post(base+'project', json = data)
 # print(res.json())
-# skill = {
-#     "skill_name": "Handling Sawaa",
-#     "skill_description": "Cutting down treese to clear the way. ",
-# }
-# res = requests.post(base2 + 'skill', json = skill)
-# print(res.json())
+skill = {
+    "skill_name": "Handling Sawaa",
+    "skill_description": "Cutting down treese to clear the way. ",
+}
+res = requests.post(base2 + 'skill', json = skill)
+print(res.json())
 
 # # data = {
 # #     "username": "nyudemo"
@@ -32,8 +32,8 @@ base2 = "https://resq-api.azurewebsites.net/api/"
 #     "skills": {"handling Saw":5,"Handling Axe":1}
 # }
 # res = requests.post(base + 'vol', json = data)
-res = requests.get(base + 'vol')
-print(res.json())
+# res = requests.get(base + 'vol')
+# print(res.json())
 
 # res = requests.get(base2 + 'quantum/sim')
 # print(res.json())
