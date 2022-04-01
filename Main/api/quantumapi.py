7e15a4eec8ebc50f70ed9de731f63709c9e77768
anushkrishnav <anush.venkatakrishna@gmail.com>
2022-04-01 15:16:13 +0530

    final

--- Main/api/quantumapi.py
+++ Main/api/quantumapi.py
@@ -1,22 +1,27 @@
 from flask import Flask, Response, request, send_file
-from flask_mongoengine import json
 from Main.Model.Skill import Project
 from Main.Model.Volunteer import VUser
 from Main.Model.User import User
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 import json
-
-class Quantapi(Resource):
-    def post():
+from Main.api.utils.sim import main
+class Quantumapi(Resource):
+    def get(self):
         volunteers = VUser.objects.all()
         volunteer_dict = {}
         for i in volunteers:
             volunteer_dict[i.username] = i.skills
+        print(volunteer_dict)
         jobs = Project.objects.all()
         job_dict = {}
         for i in jobs:
-            job_dict[i.project_name] = i.skills
-        return volunteer_dict
+            job_dict[i.project_name] = (10, i.skills)
+        print(job_dict)
+        print(volunteer_dict)
+        alocation = main(volunteer_dict, job_dict, False)
+        return Response(json.dumps(alocation), mimetype='application/json')
+
+
 
 
--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import requests
 # data = {
 #     "username": "nyudemo",
-#     "project_name": "123aa7",
-#     "project_description": "12aa37",
+#     "project_name": "job2",
+#     "project_description": "jb2",
 #     "project_reward": 1267,
 #     "skills": {
-#         "Handling Axe": 5,
+#         "Handling Saw": 4,
 #     } 
 #     }
 base = "http://127.0.0.1:5000/api/"
@@ -13,25 +13,27 @@ base2 = "https://resq-api.azurewebsites.net/api/"
 # res = requests.post(base+'project', json = data)
 # print(res.json())
 # skill = {
-#     "skill_name": "Handling Axe",
+#     "skill_name": "Handling Saw",
 #     "skill_description": "Cutting down treese to clear the way. ",
 # }
 # res = requests.post(base + 'skill', json = skill)
 # print(res.json())
 
+# # data = {
+# #     "username": "nyudemo"
+# # }
+# # res = requests.get(base + 'user/project', json = data)
+# # print(res.json())
+
 # data = {
-#     "username": "nyudemo"
+#     "username": "vol2",
+#     "email": "vol2@gmail.com",
+#     "name": "vol2",
+#     "skills": {"handling Saw":5,"Handling Axe":1}
 # }
-# res = requests.get(base + 'user/project', json = data)
+# res = requests.post(base + 'vol', json = data)
+# # # res = requests.get(base2 + 'vol')
 # print(res.json())
 
-data = {
-    "username": "vol1",
-    "email": "vol1@gmail.com",
-    "name": "vol1",
-    "skills": {"Handling Axe":1}
-}
-import json
-res = requests.post(base + 'vol', json = data)
-# res = requests.get(base2 + 'vol')
+res = requests.get(base + 'quantum/sim')
 print(res.json())
